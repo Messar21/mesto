cdaa72dd94f2c6f60f47b7eebcffaa28222a26ad
Anton Geras <jutsio@yandex.ru>
2022-11-22 22:26:45 +0300

    feat: добавить стили при ошибке валидации v2

---
+++ scripts/validate.js
@@ -0,0 +1,11 @@
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__save',
+    inactiveButtonClass: 'popup__save_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__input-error_visible'
+});
