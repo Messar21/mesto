084381871368623a2e8656178bcdd891590c43af
Anton Geras <jutsio@yandex.ru>
2023-02-08 00:43:01 +0300

    Удалить неиспользуемые константы

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,32 +4,6 @@ const btnAddCard = profile.querySelector('.profile__add-button');
 
 const btnEditAvatar = profile.querySelector('.profile__avatar-btn');
 
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
 
 const options = {
     formSelector: '.popup__form',
@@ -40,4 +14,4 @@ const options = {
     errorClass: 'popup__input-error_visible'
 };
 
-export { btnEditProfile, btnAddCard, initialCards, options, btnEditAvatar };
+export { btnEditProfile, btnAddCard, options, btnEditAvatar };
